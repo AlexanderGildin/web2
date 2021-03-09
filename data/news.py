20a1c855abc15cc7d3345fcd6303a1faa40ac138
Alexander Gildin <sgildin@mail.ru>
2021-03-09 17:32:23 +0500

    отображаются новости

--- data/news.py
+++ data/news.py
@@ -12,8 +12,8 @@ class News(SqlAlchemyBase):
                            primary_key=True, autoincrement=True)
     title = sqlalchemy.Column(sqlalchemy.String, nullable=True)
     content = sqlalchemy.Column(sqlalchemy.String, nullable=True)
-    created_date = sqlalchemy.Column(sqlalchemy.DateTime,
-                                     default=datetime.datetime.now)
+    created_date = sqlalchemy.Column(sqlalchemy.DATE,
+                                     default=datetime.date)
     is_private = sqlalchemy.Column(sqlalchemy.Boolean, default=True)
 
     user_id = sqlalchemy.Column(sqlalchemy.Integer,
